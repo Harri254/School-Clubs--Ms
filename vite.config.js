9725fd1baae8ede175f5cad4c4d965194b255b26
Katu <harrison2002sirya@gmail.com>
2025-03-18 10:34:30 +0300

    Fixed deployment issues

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/School-Clubs--Ms/", // This ensures correct asset loading on GitHub Pages
+  base: "/School-Clubs--Ms/", // Matches GitHub repository name
 });
